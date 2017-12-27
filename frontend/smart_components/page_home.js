515c336694b88e30b3896d7099d55f2d83c20d11
dragosiordachioaia <xdragosy@gmail.com>
2017-12-27 14:37:07 +0000

    changed image

--- frontend/smart_components/page_home.js
+++ frontend/smart_components/page_home.js
@@ -10,7 +10,7 @@ export default smartComponent(
         <div className="page-home">
           <div className="hero">
             <img
-              src="https://lh4.googleusercontent.com/zT3YZjlpgp6RilW_mzWEKWg_tmlucKzHdLZi_0NDLosYPdWm7jB9zyYdwRwSLp0X8SQFfEM5-t8nX_JvqWFe=w3360-h1592-rw"
+              src="https://storage.googleapis.com/digitalmonkeys_shop_1/shop-bg-2.jpg"
             />
             <h1>Boxing Day Sale</h1>
             <Link to="/boxing_day_sale">
